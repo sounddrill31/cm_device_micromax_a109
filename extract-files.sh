2c8e91b5247a660a393d6ba83bab8fc32a4886d7
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-04-30 13:16:37 +0530

    extract-files.sh: Take arguments instead of ssh

--- extract-files.sh
+++ extract-files.sh
@@ -4,6 +4,21 @@ VENDOR=micromax
 DEVICE=A109
 
 BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+if [ $# -eq 0 ]; then
+  SRC=adb
+else
+  if [ $# -eq 1 ]; then
+    SRC=$1
+  else
+    echo "$0: bad number of arguments"
+    echo ""
+    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+    echo ""
+    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
+    echo "the device using adb pull."
+    exit 1
+  fi
+fi
 
 echo "Pulling $DEVICE files..."
 for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
@@ -12,7 +27,7 @@ DIR=`dirname $FILE`
 mkdir -p $BASE/$DIR
     fi
 
-cp system/$FILE $BASE/$FILE
+cp $SRC/system/$FILE $BASE/$FILE
 
 #adb pull /system/$FILE $BASE/$FILE
 done
